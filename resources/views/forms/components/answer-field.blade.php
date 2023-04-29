3668b1d19166db459b0c553cb6b1c5a8495a2c43
teiuqpeek <gurdin.k@gmail.com>
2023-04-29 17:02:29 +0300

    update

--- resources/views/forms/components/answer-field.blade.php
+++ resources/views/forms/components/answer-field.blade.php
@@ -15,9 +15,10 @@
     <div x-data="{ state: $wire.entangle('{{ $getStatePath() }}').defer }" class="text-md py-3 grid">
         <!-- Interact with the `state` property in Alpine.js -->
 
-        <label for="" class="text-grey-700 font-semibold text-md mb-3">{{ $getLabel() }}</label>
-
-        <input wire:model.defer="{{ $getStatePath() }}" class="p-0 bg-transparent w-full" / disabled>
+        <p for="" class="text-grey-700 font-semibold text-md mb-3">{{ $getLabel() }}</p>
+        {{-- {{ dd($getStatePath()) }} --}}
+        {{-- <input wire:model.defer="{{ $getStatePath() }}" class="p-0 bg-transparent w-full" / disabled> --}}
+        <p wire:model.defer="{{ $getStatePath() }}">{{ $getState() }}</p>
 
     </div>
     <hr>
--- resources/views/forms/components/hero.blade.php
+++ resources/views/forms/components/hero.blade.php
@@ -12,8 +12,9 @@
     :state-path="$getStatePath()">
 
     <div class="py-3">
-        <label for="{{ $getId() }}" class="font-bold"></label>
-        <input wire:model.defer="{{ $getStatePath() }}" id="{{ $getId() }}" class="p-0 bg-transparent font-bold text-3xl" / disabled>
+        {{-- <label for="{{ $getId() }}" class="font-bold"></label> --}}
+        {{-- <input wire:model.defer="{{ $getStatePath() }}" id="{{ $getId() }}" class="p-0 bg-transparent font-bold text-3xl" / disabled> --}}
+        <p wire:model.defer="{{ $getStatePath() }}" id="{{ $getId() }}" class="p-0 bg-transparent font-bold md:text-3xl text-2xl">{{ $getState() }}</p>
     </div>
     <hr>
 </x-dynamic-component>
